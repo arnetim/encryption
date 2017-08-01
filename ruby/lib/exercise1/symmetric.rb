5c2fff43c7262ee5dc57d835cc02ff8dce773f17
arnetim <arne@xs4all.nl>
2017-08-01 14:19:35 +0200

    use SecretBox

--- ruby/lib/exercise1/symmetric.rb
+++ ruby/lib/exercise1/symmetric.rb
@@ -1,12 +1,12 @@
 module Symmetric
 
   def self.encrypt(key, message)
-    box = RbNaCl::RandomNonceBox.from_secret_key(key)
+    box = RbNaCl::SecretBox.from_secret_key(key)
     box.box(message)
   end
 
   def self.decrypt(key, ciphertext)
-    box = RbNaCl::RandomNonceBox.from_secret_key(key)
+    box = RbNaCl::SecretBox.from_secret_key(key)
     box.open(ciphertext)
   end
 
