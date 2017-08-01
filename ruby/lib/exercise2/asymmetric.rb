2f56d9f76066be45390df6e9816cc42e366eb93c
arnetim <arne@xs4all.nl>
2017-08-01 14:20:29 +0200

    use SecretBox

--- ruby/lib/exercise2/asymmetric.rb
+++ ruby/lib/exercise2/asymmetric.rb
@@ -1,13 +1,13 @@
 module Asymmetric
 
   def self.encrypt(receiver_public_key, sender_private_key, message)
-    box = RbNaCl::RandomNonceBox.from_keypair(receiver_public_key, sender_private_key)
-    box.box(message)
+    box = RbNaCl::SecretBox.from_keypair(receiver_public_key, sender_private_key)
+    box.encrypt(message)
   end
 
   def self.decrypt(receiver_private_key, sender_public_key, ciphertext)
-    box = RbNaCl::RandomNonceBox.from_keypair(sender_public_key, receiver_private_key)
-    box.open(ciphertext)
+    box = RbNaCl::SecretBox.from_keypair(sender_public_key, receiver_private_key)
+    box.decrypt(ciphertext)
   end
 
 end
