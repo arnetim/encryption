826a8e9f8e1af305ad449f7042842c34670c181d
arnetim <arne@xs4all.nl>
2017-08-02 14:26:02 +0200

    use SimpleBox

--- ruby/lib/exercise2/asymmetric.rb
+++ ruby/lib/exercise2/asymmetric.rb
@@ -1,12 +1,12 @@
 module Asymmetric
 
   def self.encrypt(receiver_public_key, sender_private_key, message)
-    box = RbNaCl::SecretBox.encrypt(receiver_public_key, sender_private_key)
+    box = RbNaCl::SimpleBox.from_keypair(receiver_public_key, sender_private_key)
     box.encrypt(message)
   end
 
   def self.decrypt(receiver_private_key, sender_public_key, ciphertext)
-    box = RbNaCl::SecretBox.decrypt(sender_public_key, receiver_private_key)
+    box = RbNaCl::SimpleBox.from_keypair(sender_public_key, receiver_private_key)
     box.decrypt(ciphertext)
   end
 
