f70658dbb2c4e6f8f6abc116c964261dfa8317e9
arnetim <arne@xs4all.nl>
2017-08-02 14:25:21 +0200

    use SimpleBox

--- ruby/lib/exercise1/symmetric.rb
+++ ruby/lib/exercise1/symmetric.rb
@@ -1,12 +1,12 @@
 module Symmetric
 
   def self.encrypt(key, message)
-    box = RbNaCl::SecretBox.from_secret_key(key)
+    box = RbNaCl::SimpleBox.from_secret_key(key)
     box.box(message)
   end
 
   def self.decrypt(key, ciphertext)
-    box = RbNaCl::SecretBox.from_secret_key(key)
+    box = RbNaCl::SimpleBox.from_secret_key(key)
     box.open(ciphertext)
   end
 
